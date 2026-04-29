90f445f6e0f9521a3282c9b708078440842ef8b0
Welleson Torquato <welleson@jardim.adm.local>
2026-04-29 13:25:23 -0300

    Configura Netlify Identity

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,30 @@
 import "@/styles/globals.css";
+import Script from "next/script";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", (user) => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+
+      window.netlifyIdentity.init();
+    }
+  }, []);
+
+  return (
+    <>
+      <Script
+        src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+        strategy="afterInteractive"
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
