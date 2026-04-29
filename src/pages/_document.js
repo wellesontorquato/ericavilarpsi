41562abf139206d6f1d9e15587f65ebc38ac500c
Welleson Torquato <welleson@jardim.adm.local>
2026-04-29 13:18:18 -0300

    Ajuste Favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,10 @@ export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
-        {/* Fontes do Google devem ficar exclusivamente aqui no Document */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
@@ -12,6 +15,7 @@ export default function Document() {
           rel="stylesheet"
         />
       </Head>
+
       <body>
         <Main />
         <NextScript />
